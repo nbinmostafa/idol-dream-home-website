9475e04154e4c5195fc4d51a8fdfb26d659848ac
nbinmostafa <nbinmostafa@gmail.com>
2025-05-19 22:35:32 -0400

    Home component: added ProjectsGrid section and cleaned up formatting

--- idol-dream-homes/src/Pages/Home.jsx
+++ idol-dream-homes/src/Pages/Home.jsx
@@ -1,16 +1,17 @@
 import Footer from "../Components/Footer/Footer.jsx";
-import ContactForm from "../Components/ContactPage/ContactForm.jsx";
 import Hero from "../Components/HomePage/Hero.jsx";
 import Metrics from "../Components/HomePage/Metrics.jsx";
 import About from "../Components/HomePage/About.jsx";
 import AboutExtended from "../Components/HomePage/AboutExtended.jsx";
 import GetInTouch from "../Components/HomePage/GetInTouch.jsx";
+import ProjectsGrid from "../Components/ProjectsPage/ProjectsGrid.jsx";
 
 function Home() {
   return (
     <div className="home-page">
+      
       <section className="hero-section">
-        <Hero />
+        <Hero/>
       </section>
 
       <section className="metrics-section container fade-in">
@@ -20,6 +21,12 @@ function Home() {
       <section className="about-section container fade-in">
         <About />
       </section>
+
+      <section className="projects-section container fade-in">
+        <ProjectsGrid />
+      </section>
+
+
       
       <section className="about-extended-section container fade-in">
         <AboutExtended />
